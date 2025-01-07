80eee53d62b9580ac22cd8a5d811a47b80676858
Jeegar Goyani <jigo961257@gmail.com>
2025-01-07 10:11:16 +0530

    update-action-login

--- src/zustand/login/action.tsx
+++ src/zustand/login/action.tsx
@@ -1,14 +1,18 @@
+import { API_URL } from '@/constants/Constant';
 import axios from 'axios'
 
-export const signin = async (data) => {
+export const signin = async (data: any) => {
     try {
         const response = await axios.post(
-            `https://pleased-presumably-egret.ngrok-free.app/auth/login`,
+            `${API_URL}/auth/login`,
             data,
         )
         return response?.data;
     } catch (err) {
-        return err?.response?.data;
+        if (axios.isAxiosError(err) && err.response) {
+            return err.response.data;
+        }
+        return { message: 'An unknown error occurred' };
 
     }
 }
